8f9c1d2223d1d8e6b7a1278f456e5aba2f8004d7
Marcilio Júnior <jrsmarcilio@gmail.com>
2022-01-22 15:54:02 -0300

    updates axios post in login

--- src/components/FormLogin.tsx
+++ src/components/FormLogin.tsx
@@ -5,21 +5,8 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import { IUserLogin } from "../interfaces/Students";
 import { api } from "../service/api";
 import { toast } from "react-toastify";
-import { useRouter } from "next/router";
-
-import { GetServerSideProps } from "next";
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { req, res } = ctx;
-
-  const { cookies } = req;
-  console.log(cookies);
-  return { props: {} };
-};
 
 export default function FormLogin() {
-  const router = useRouter();
-
   const {
     register,
     handleSubmit,
@@ -28,16 +15,10 @@ export default function FormLogin() {
 
   const onSubmit: SubmitHandler<IUserLogin> = (data) => {
     api
-      .post("/login", {
-        data,
-        withCredentials: true,
-      })
+      .post("/login", data)
       .then((response) => {
-        console.log(response);
-        // if (response.status === 200) {
-        //   toast.success("Access successfully!");
-        //   router.push("/students");
-        // }
+        console.log(response.data);
+        toast.success("Access successfully!");
       })
       .catch((error) => {
         console.error(error);
@@ -68,6 +49,7 @@ export default function FormLogin() {
           <TextField
             type="password"
             placeholder="Password"
+            autoComplete="current-password"
             fullWidth
             {...register("password", { required: true })}
           />
--- src/service/api.ts
+++ src/service/api.ts
@@ -2,5 +2,4 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://school-ads.herokuapp.com",
-  withCredentials: true,
 });
