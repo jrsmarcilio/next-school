2b60ca8962aa0967b0f11edf5f3e4ecea16a79cf
Marcilio Júnior <jrsmarcilio@gmail.com>
2022-01-22 13:18:03 -0300

    updates axios config

--- src/components/FormLogin.tsx
+++ src/components/FormLogin.tsx
@@ -7,6 +7,16 @@ import { api } from "../service/api";
 import { toast } from "react-toastify";
 import { useRouter } from "next/router";
 
+import { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { req, res } = ctx;
+
+  const { cookies } = req;
+  console.log(cookies);
+  return { props: {} };
+};
+
 export default function FormLogin() {
   const router = useRouter();
 
@@ -18,8 +28,12 @@ export default function FormLogin() {
 
   const onSubmit: SubmitHandler<IUserLogin> = (data) => {
     api
-      .post("/login", data)
+      .post("/login", {
+        data,
+        withCredentials: true,
+      })
       .then((response) => {
+        console.log(response);
         if (response.status === 200) {
           toast.success("Access successfully!");
           router.push("/students");
