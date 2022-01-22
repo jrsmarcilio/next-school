ff6a5b1e9d2921cd60299738a7c0639edfd9f00b
Marcilio Júnior <jrsmarcilio@gmail.com>
2022-01-22 18:29:35 -0300

    remove cors anywhere heroku

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL:
-    "https://cors-anywhere.herokuapp.com/https://school-ads.herokuapp.com",
+    "https://school-ads.herokuapp.com",
 });
