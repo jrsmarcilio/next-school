ac7e7f82e32ae788c968eaf7e8ed630b37a0c772
Marcilio Júnior <jrsmarcilio@gmail.com>
2022-01-22 22:20:14 -0300

    deploy vercel

--- src/components/FormLogin.tsx
+++ src/components/FormLogin.tsx
@@ -13,8 +13,8 @@ export default function FormLogin() {
     formState: { errors },
   } = useForm<IUserLogin>();
 
-  const onSubmit: SubmitHandler<IUserLogin> = async (data) => {
-    await api
+  const onSubmit: SubmitHandler<IUserLogin> = (data) => {
+    api
       .post("/login", data)
       .then((response) => {
         const token = response.headers["set-cookie"];
